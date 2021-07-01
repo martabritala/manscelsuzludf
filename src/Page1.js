de1a862017ec95e0e993d3912458401cb5042b20
martabritala <53839047+martabritala@users.noreply.github.com>
2021-07-01 17:03:56 +0300

    punkts??

--- src/Page1.js
+++ src/Page1.js
@@ -19,8 +19,8 @@ const heading = {
 const mapLink = 'https://www.openstreetmap.org/?mlat=56.93866&mlon=24.09571#map=17/56.93866/24.09571&layers=H';
 
 const hero = {
-  lv: "/images/majas1.png",
-  en: "/images/majas1EN.png",
+  lv: "./images/majas1.png",
+  en: "./images/majas1EN.png",
 };
 
 const content = {
