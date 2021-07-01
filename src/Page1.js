3990dcb93de31a1fbcc9cd8d9e22f51d8ba66ee6
martabritala <53839047+martabritala@users.noreply.github.com>
2021-07-01 08:24:21 +0300

    izmeri

--- src/Page1.js
+++ src/Page1.js
@@ -66,7 +66,7 @@ export default function HomePage() {
     <${Main}
       style=${{
         background: `
-          url(./images/subpat.circuit.png) center center / 800px repeat,
+          url(./images/fons.png) center center / 800px repeat,
           var(--header-bg-blue)
         `,
       }}
