42db58bf8991a6890cde41a158f1fae954efae66
martabritala <53839047+martabritala@users.noreply.github.com>
2021-07-02 00:35:11 +0300

    letstry google maps

--- src/Page1.js
+++ src/Page1.js
@@ -48,7 +48,8 @@ const content = {
       When I was little the name of the building was still visible on the wall.
       Currenty we have left Rīgas Namu Pārvaldnieks and have made a union to renovate our home. 
     <//>
-    <${MapImage} src="./images/map.akmenu.png" href=${mapLink} />
+    <iframe src="https://www.google.com/maps/embed?pb=!1m56!1m12!1m3!1d2554.763657950183!2d24.11266605874542!3d56.94429685694671!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m41!3e2!4m5!1s0x46eed02f16154351%3A0xf48a69299f5788a1!2zQWttZcWGdSBpZWxhIDIyLCBaZW1nYWxlcyBwcmlla8WhcGlsc8STdGEsIFLEq2dhLCBMVi0xMDQ4LCBMYXR2aWph!3m2!1d56.938660399999996!2d24.0957306!4m3!3m2!1d56.9409546!2d24.0965249!4m5!1s0x46eed02815f1d127%3A0x30a1fb9c59059693!2sAkmens%20tilts!3m2!1d56.944773899999994!2d24.1013625!4m5!1s0x46eecfd7a81ed531%3A0x6bec487673a5b6ae!2zUlRVLCBLYcS8xLd1IGllbGEsIENlbnRyYSByYWpvbnMsIFLEq2dh!3m2!1d56.9472011!2d24.1051693!4m5!1s0x46eecfd6962df1b3%3A0xd2275fc056cc9eec!2zUsSrZ2FzIFN2LiBQxJN0ZXJhIGJhem7Eq2NhLCBSZWZvcm3EgWNpamFzIExhdWt1bXMsIENlbnRyYSByYWpvbnMsIFLEq2dh!3m2!1d56.947533199999995!2d24.108645199999998!4m5!1s0x46eecfd3e3e99037%3A0xd7e42c25953bdc3b!2s%C3%92pera%20Nacional%20de%20Let%C3%B2nia%2C%20Aspazijas%20bulv%C4%81ris%2C%20Centra%20rajons%2C%20R%C4%ABga!3m2!1d56.949433299999995!2d24.113591!4m5!1s0x46eecfd3cdaa69c7%3A0x16a2b44d483ec349!2zTGF0dmlqYXMgVW5pdmVyc2l0xIF0ZSwgUmFpxYZhIGJ1bHbEgXJpcywgQ2VudHJhIHJham9ucywgUsSrZ2E!3m2!1d56.950809799999995!2d24.1163132!5e0!3m2!1slv!2slv!4v1625174941626!5m2!1slv!2slv" 
+    width="77vh" height="35vh" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     <${P}>
       There is more information about the building in the documentary series „Ielas garumā”.
     <//>
